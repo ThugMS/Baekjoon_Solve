f582001d8d9971349d8a04d00a3779b2a752b220
MSLEE <leems041625@gmail.com>
2022-07-03 01:45:39 +0900

    2483(세 용액).cpp solve

---
+++ 2483(세 용액).cpp
@@ -0,0 +1,62 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <cmath>
+//
+//#pragma warning(disable:4996)
+//
+//using namespace std;
+//
+//
+//int main() {
+//
+//	int N;
+//
+//	scanf("%d", &N);
+//
+//	vector<long long> v;
+//	
+//	for (int i = 0; i < N; i++) {
+//		long long a;
+//		cin >> a;
+//		v.push_back(a);
+//	}
+//
+//	sort(v.begin(), v.end());
+//
+//	long long ans1;
+//	long long ans2;
+//	long long ans3;
+//	long long sum = 30000000001;
+//
+//	for (int i = 0; i < N - 2; i++) {
+//		long long start = i+1;
+//		long long end = N - 1;
+//
+//		while (start < end) {
+//			if (sum >= (long long)abs(v[start] + v[end] + v[i])) {
+//				sum = (long long)abs(v[start] + v[end] + v[i]);
+//				ans1 = v[i];
+//				ans2 = v[start];
+//				ans3 = v[end];
+//			}
+//			if (v[start] + v[end] + v[i] > 0) {
+//				end--;
+//			}
+//			else if (v[start] + v[end] + v[i] < 0) {
+//				start++;
+//			}
+//			else {
+//				ans1 = v[i];
+//				ans2 = v[start];
+//				ans3 = v[end];
+//				break;
+//			}
+//		}
+//		if (sum == 0)break;
+//	}
+//
+//	cout << ans1 << " " << ans2 <<  " " << ans3 << endl;
+//
+//	return 0;
+//}
