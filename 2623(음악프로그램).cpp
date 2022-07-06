b00fa62aeaeca529cfc444cfe1d6626bb7ca3900
LeeMinSeok <leems041625@gmail.com>
2022-07-06 16:32:07 +0900

    2623(음악프로그램) solve

--- 2623(음악프로그램).cpp
+++ 2623(음악프로그램).cpp
@@ -1,61 +1,79 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-#include <cstring>
-
-#pragma warning(disable:4996)
-
-using namespace std;
-
-
-int main() {
-
-	int N;
-	
-		scanf("%d", &N);
-	
-		vector<long long> v;
-		
-		for (int i = 0; i < N; i++) {
-			long long a;
-			cin >> a;
-			v.push_back(a);
-		}
-	
-		sort(v.begin(), v.end());
-	
-		long long ans1;
-		long long ans2;
-		long long ans3;
-		long long sum = 30000000001;
-	
-		for (int i = 0; i < N - 2; i++) {
-			long long start = i+1;
-			long long end = N - 1;
-	
-			while (start < end) {
-				if (sum >= (long long)abs(v[start] + v[end] + v[i])) {
-					sum = (long long)abs(v[start] + v[end] + v[i]);
-					ans1 = v[i];
-					ans2 = v[start];
-					ans3 = v[end];
-				}
-				if (v[start] + v[end] + v[i] > 0) {
-					end--;
-				}
-				else if (v[start] + v[end] + v[i] < 0) {
-					start++;
-				}
-				else {
-					ans1 = v[i];
-					ans2 = v[start];
-					ans3 = v[end];
-					break;
-				}
-			}
-			if (sum == 0)break;
-		}
-
-	return 0;
-}
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <cmath>
+//#include <queue>
+//#include <cstring>
+//
+//#pragma warning(disable:4996)
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int n, m;
+//
+//	scanf("%d %d", &n, &m);
+//
+//	vector<int> v[1010];
+//	
+//	vector<int> inDegree(n+1);
+//
+//	vector<int> ans;
+//
+//	for (int i = 0; i < m; i++) {
+//		int s;
+//		scanf("%d", &s);
+//		vector<int> sub;
+//		for (int j = 0; j < s; j++) {
+//			int s;
+//			scanf("%d", &s);
+//			sub.push_back(s);
+//		}
+//
+//		for (int j = 0; j < sub.size(); j++) {
+//			for (int k = j + 1; k < sub.size(); k++) {
+//				v[sub[j]].push_back(sub[k]);
+//				inDegree[sub[k]]++;
+//			}
+//		}
+//	}
+//
+//	//cout << "yes" << endl;
+//
+//	queue<int> q;
+//
+//	for (int i = 1; i <= n; i++) {
+//		if (inDegree[i] == 0) {
+//			q.push(i);
+//		}
+//	}
+//
+//	while (!q.empty()) {
+//		int cur = q.front();
+//		q.pop();
+//		ans.push_back(cur);
+//
+//		for (int i = 0; i < v[cur].size(); i++) {
+//			inDegree[v[cur][i]]--;
+//
+//			if (inDegree[v[cur][i]] == 0) {
+//				q.push(v[cur][i]);
+//			}
+//		}
+//
+//	}
+//	
+//	if (ans.size() != n) {
+//		cout << 0 << endl;
+//	}
+//	else {
+//		for (int i = 0; i < ans.size(); i++) {
+//			cout << ans[i] << endl;
+//		}
+//	}
+//	
+//
+//
+//	return 0;
+//}
